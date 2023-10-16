9facca41ac6e0645fbfc7303a7c02182e1da81f7
Shandar Denys <msden.ua@gmail.com>
2023-10-16 23:22:36 +0300

    read and write file in sync mode

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello world')
+const fs = require('fs')
+
+/** @type {(root: string) => (file: string) => void} */
+const get = root => file => {
+  try {
+    const data = fs.readFileSync(`cdt0/${root}`)
+    if (data[0] === 32) {
+      const out = data.subarray(1)
+      fs.writeFileSync(file, out)
+    } else {
+      console.error('not implemented')
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+get('mnb8j83rgrch8hgb8rbz28d64ec2wranzbzxcy4ebypd8')('out')
