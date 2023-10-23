57b562430b2a9900419d0a525336785bb364d458
Shandar Denys <msden.ua@gmail.com>
2023-10-23 21:21:40 +0300

    add simple test

---
+++ test.js
@@ -0,0 +1,11 @@
+const { getParityBit } = require('./index')
+
+{
+  const parity = getParityBit(0n)
+  if (parity !== 0) { console.error(parity) }
+}
+
+{
+  const parity = getParityBit(1n)
+  if (parity !== 1) { console.error(parity) }
+}
