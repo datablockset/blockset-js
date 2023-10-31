2a19b81727d607ba3275c6bb66173f470f9ad761
Shandar Denys <msden.ua@gmail.com>
2023-10-31 22:01:17 +0200

    nextState

--- index.mjs
+++ index.mjs
@@ -3,6 +3,10 @@ import base32 from './base32.mjs'
 import tree from './tree.mjs'
 import digest256 from './digest256.mjs'
 /** @typedef {import('./tree.mjs').State} StateTree */
+/**
+ * @template T
+ * @typedef {import('./subtree.mjs').Nullable<T>} Nullable
+ */
 const { toAddress } = base32
 const { push: pushTree, end: endTree, partialEnd: partialEndTree, pushDigest } = tree
 const { tailToDigest } = digest256
@@ -13,14 +17,8 @@ const { tailToDigest } = digest256
  */
 
 /**
- * @typedef {readonly [Address, Uint8Array]} Block
+ * @typedef {[Address, Uint8Array]} Block
  */
-
-/**
- * @typedef {{
-*  readonly blocks: Block[]
-* }} Input
-*/
 
 /**
  * @template T
@@ -39,11 +37,19 @@ const { tailToDigest } = digest256
  */
 
 /**
- * @typedef {readonly [Uint8Array, readonly Address[]]} OkOutput
+ * @typedef {readonly [Nullable<Uint8Array>, Nullable<Address>]} OkOutput
  */
 
 /**
  * @typedef { Result<OkOutput,string> } Output
+*/
+
+/**
+ * @typedef {[Address, Nullable<Uint8Array>]} BlockState
+ */
+
+/**
+ * @typedef { BlockState[] } State
 */
 
 /** @type {(address: Address) => string} */
@@ -78,16 +84,53 @@ const getBuffer = ([address, isRoot]) => {
       if (typeof childBuffer === 'string') {
         return childBuffer
       }
-      result = new Uint8Array([ ...result, ...childBuffer ]);
+      result = new Uint8Array([...result, ...childBuffer]);
     }
     pushDigest(verificationTree)(tailToDigest(tail))
-    result = new Uint8Array([ ...result, ...tail ]);
+    result = new Uint8Array([...result, ...tail]);
   }
   const digest = isRoot ? endTree(verificationTree) : partialEndTree(verificationTree)
   if (digest === null || toAddress(digest) !== address) {
     return address
   }
   return result
+}
+
+/** @type {(state: State) => (block: Block) => boolean} */
+const insertBlock = state => block => {
+  for(let i = 0; i < state.length; i++) {
+    if (state[i][0][0] === block[0][0]) {
+      state[i][1] = block[1]
+      return true
+    }
+  }
+  return false
+}
+
+/** @type {(state: State) => (block: Block) => Output} */
+const nextState = state => block => {
+  if (state.length === 0) {
+    state.push(block)
+  } else if (!insertBlock(state)(block)) {
+    return ['error', 'unknown address']
+  }
+
+  const blockLast = state.at(-1)
+  if (blockLast === undefined) {
+    throw 'unexpected behaviour'
+  }
+  const data = blockLast[1]
+  if(data === null) {
+    return ['ok', [null, blockLast[0]]]
+  }
+
+  state.pop()
+
+  //todo: implement logic from getBuffer
+  //todo: push hashes as addresses to state
+  //todo: add tail to State as BlockState with reserved address
+
+  throw 'not implemented'
 }
 
 /** @type {(root: string) => (file: string) => number} */
