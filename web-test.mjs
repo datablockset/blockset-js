33e31a7ff74ad09533bf4c7a8b6eae74524b89e3
Shandar Denys <msden.ua@gmail.com>
2023-11-08 23:02:07 +0200

    call get

--- web-test.mjs
+++ web-test.mjs
@@ -1,11 +1,15 @@
 import getModule from './get.mjs'
-const { get } = getModule
+const { get, fetchRead } = getModule
 
 // @ts-ignore
 document.getElementById('download').addEventListener('click', () => {
   // @ts-ignore
   const hash = document.getElementById('input').value
-  //TODO: call get
+  let buffer = new Uint8Array()
+  const read = fetchRead('410f5a49.blockset-js-test.pages.dev')
+  /** @type {(b: Uint8Array) => Promise<void>} */
+  const write = async(b) => { buffer = new Uint8Array([...buffer, ...b]) }
+  const exitCode = get({read, write})(hash)
   // @ts-ignore
-  document.getElementById('output').innerHTML = 'not implemented'
+  document.getElementById('output').innerHTML = `exit code = ${exitCode}`
 });
