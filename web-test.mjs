62b198b682b5e1cceed8745c28ca1fe3c8508af8
Shandar Denys <msden.ua@gmail.com>
2023-11-08 22:41:39 +0200

    add index.html

---
+++ web-test.mjs
@@ -0,0 +1,8 @@
+// @ts-ignore
+document.getElementById('download').addEventListener('click', () => {
+  // @ts-ignore
+  const hash = document.getElementById('input').value
+  //TODO: call get
+  // @ts-ignore
+  document.getElementById('output').innerHTML = 'not implemented'
+});
