c5c32a49f6f2eae15fffa4f0368ed6709648d416
Shandar Denys <msden.ua@gmail.com>
2023-11-14 19:04:02 +0200

    publish script

--- publish.mjs
+++ publish.mjs
@@ -1,4 +1,29 @@
 import fs from 'node:fs'
+import childProcess from 'node:child_process'
+import { promisify } from 'node:util'
 
-const pjson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-console.log(pjson.version)
+const exec2 = promisify(childProcess.exec)
+
+/** @type {(cmd: string) => Promise<void>} */
+const exec = async(cmd) => {
+  await exec2(cmd).then(({stdout, stderr}) => {
+    console.log(`exec ${cmd}`)
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+
+    console.log(`stdout:\n${stdout}`);
+  }).catch(reason => console.error(`error: ${reason.message}`))
+}
+
+const publish = async () => {
+  const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version
+  console.log(`publishing version ${version}`)
+  await exec('npm publish')
+  await exec(`git tag -a v${version} -m 'v${version}'`)
+  await exec('git push origin --tags')
+}
+
+publish()
